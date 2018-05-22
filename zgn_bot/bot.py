33290372707b1c8a33bc14c41ab8888720e366e2
zgn <moskalev.d.a@gmail.com>
2018-05-22 18:40:52 +0300

    week 2nd. in work

--- zgn_bot/bot.py
+++ zgn_bot/bot.py
@@ -1,17 +1,46 @@
-from telegram.ext import Updater, CommandHandler
+# coding=utf-8
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+import logging
+import ephem
 
-def start_bot(bot, updater):
-    tymext = """Привет, пользователь!
-Я простой бот и понимаю только команду /start
-"""
-    update.message.reply_text(mytext)
-    ptint("start")
+PROXY = {'proxy_url': 'socks5://t1.learn.python.ru:1080',
+    'urllib3_proxy_kwargs': {'username': 'learn', 'password': 'python'}}
+
+logging.basicConfig(format='%(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO,
+                    filename='bot.log'
+                    )
+
 
 def main():
-    updtr = Updater('559166681:AAETkAV20RGQbexKJ_YVateSp8JVDNapzxA')
-    updtr.dispatcher.add_handler(CommandHandler("start", start_bot))
-    updtr.start_polling()
-    updtr.idle()
+    mybot = Updater("559166681:AAETkAV20RGQbexKJ_YVateSp8JVDNapzxA", request_kwargs=PROXY)
+    dp = mybot.dispatcher
+    dp.add_handler(CommandHandler("start", greet_user))
+    dp.add_handler(MessageHandler(Filters.text, talk_to_me))
+    dp.add_handler(CommandHandler("planet", planet))
+    mybot.start_polling()
+    mybot.idle()
+
+
+def greet_user(bot, update):
+    text = 'Вызван /start'
+    print(text)
+    update.message.reply_text(text)
+
+
+def talk_to_me(bot, update):
+    user_text = update.message.text
+    print(user_text)
+    update.message.reply_text(user_text)
+
+
+def planet(bot, update):
+    user_text = update.message.text
+    print(user_text)
+    update.message.reply_text(user_text)
+
 
 if __name__ == "__main__":
     main()
+
+
